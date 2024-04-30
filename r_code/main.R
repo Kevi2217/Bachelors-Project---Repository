d66f9d97ab41aa13396900c71c006411073c0503
Freddy D <93995722+HunterFreddy69@users.noreply.github.com>
2024-04-30 11:21:11 +0200

    Add files via upload

--- r_code/main.R
+++ r_code/main.R
@@ -20,6 +20,8 @@ library(stats)
 library(forecast)
 library(zoo)
 library(tseries)
+library(gridExtra)
+library(pracma)
 
 # OR Operator
 # |
@@ -41,19 +43,27 @@ esp <- elspotprices_14_19_data %>%
 
 esp_daily <- esp %>%
   dplyr::mutate(hourdk = as.Date(hourdk)) %>%
-  dplyr::group_by(pricearea, hourdk) %>%
-  dplyr::summarise(daily_mean_spotpricedkk = round(mean(spotpricedkk), 2),
-                   daily_mean_spotpriceeur = round(mean(spotpriceeur), 2)) %>% 
-  dplyr::filter(pricearea == "DK1")
-  # dplyr::filter(year(hourdk) == "2015")
+  dplyr::filter(hourdk >= "2019-01-01" & hourdk <= "2023-12-31") %>% 
+  dplyr::filter(pricearea == "DK1") %>% 
+  dplyr::group_by(hourdk) %>%
+  dplyr::summarise(daily_mean_spotpriceeur = round(mean(spotpriceeur), 2)) %>% 
+  dplyr::mutate(rolling_mean = cummean(daily_mean_spotpriceeur),
+                rolling_sd = sqrt(cumsum((daily_mean_spotpriceeur - rolling_mean)^2) /
+                                    seq_along((daily_mean_spotpriceeur - rolling_mean)^2)))
 
 
 # Daily price plot for all time
-ggplot(esp_daily, aes(x = hourdk,
-                       y = daily_mean_spotpriceeur)) +
-  geom_line() +
-  labs(title = "DAILY Price (ALL TIME)", x = "Time", y = "Daily Price") +
-  theme_minimal()
+ggplot(esp_daily, aes(x = hourdk)) +
+  geom_line(aes(y = daily_mean_spotpriceeur, color = "Daily prices"), size = 0.3) +
+  geom_line(aes(y = rolling_mean, color = "Rolling mean"), size = 0.3) +
+  geom_line(aes(y = rolling_sd, color = "Rolling sd"), size = 0.3) +
+  scale_color_manual(values = c("Daily prices" = "black",
+                                "Rolling mean" = "red",
+                                "Rolling sd" = "blue")) +
+  labs(title = "Average daily Price (2019-2024)", x = "Time", y = "Average Daily Price")
+
+
+
 
 esp_30day_rolling_avg_daily <- esp %>%
   dplyr::mutate(hourdk = as.Date(hourdk)) %>%
@@ -61,7 +71,8 @@ esp_30day_rolling_avg_daily <- esp %>%
   dplyr::summarise(daily_mean_spotpricedkk = round(mean(spotpricedkk), 2),
                    daily_mean_spotpriceeur = round(mean(spotpriceeur), 2)) %>% 
   dplyr::filter(pricearea == "DK1") %>% 
-  dplyr::mutate(MA_30 = zoo::rollmeanr(daily_mean_spotpriceeur, k = 30, fill = NA))
+  dplyr::mutate(MA_30 = zoo::rollmeanr(daily_mean_spotpriceeur, k = 30, fill = NA)) 
+  # dplyr::filter(hourdk >= "2020-01-01")
 
 # 30 day-rolling Daily price plot for all time
 ggplot(esp_30day_rolling_avg_daily, aes(x = hourdk,
@@ -71,26 +82,78 @@ ggplot(esp_30day_rolling_avg_daily, aes(x = hourdk,
   theme_minimal()
 
 # Converts dataframe to time series
-esp_ts <- ts(esp_daily$daily_mean_spotpriceeur, frequency = 1,
-             start = c(year(esp_daily$hourdk[1]), month(esp_daily$hourdk[1])))
-plot(esp_ts)
+esp_ts <- ts(esp_daily$daily_mean_spotpriceeur, frequency = 7,
+             start = 2019)
+autoplot(esp_ts)
 
-price_diff_ts <- ts(diff(esp_daily$daily_mean_spotpriceeur), frequency = 1,
-             start = c(year(esp_daily$hourdk[1]), month(esp_daily$hourdk[1])))
-plot(price_diff_ts)
+############################# STL of time series ###############################
+esp_mstl2 <- mstl(msts(esp_ts, seasonal.periods = c(7, 365)), iterate = 50)
+theme_options <- theme(
+  plot.title = element_text(size = 14),
+  axis.title.x = element_text(size = 12),
+  axis.title.y = element_text(size = 12),
+  axis.text = element_text(size = 10),
+  legend.title = element_blank(),
+  legend.text = element_text(size = 10))
+# Plot the decomposed components
+autoplot(esp_mstl2) +
+  ggtitle("(normal) Multiple STL Decomposition of Daily Mean Spot Prices (2019-2024)") +
+  labs(x = "Time", y = "Daily Mean Spot Price (EUR)") +
+  theme_options
+################################################################################
+# Transforming data 
+# log_diff_ts <- log(esp_ts + abs(1.5*min(esp_ts))) %>% diff() %>% diff(lag = 7)
+# autoplot(log_diff_ts)
+
+lambda <- BoxCox.lambda(esp_ts + abs(1.5*min(esp_ts)))
+esp_boxcox <- BoxCox(esp_ts + abs(1.5*min(esp_ts)), lambda) %>% diff() %>% diff(lag = 7)
+
+for (i in which(abs(esp_boxcox) >= 0.05)) {
+  # Assign the value of the previous observation to the current index
+  esp_boxcox[i] <- esp_boxcox[i - 1]
+}
+autoplot(esp_boxcox)
+
+
+############################# STL of transformed time series ###############################
+esp_mstl2 <- mstl(msts(esp_boxcox, seasonal.periods = c(7, 365)), iterate = 50)
+theme_options <- theme(
+  plot.title = element_text(size = 14),
+  axis.title.x = element_text(size = 12),
+  axis.title.y = element_text(size = 12),
+  axis.text = element_text(size = 10),
+  legend.title = element_blank(),
+  legend.text = element_text(size = 10))
+# Plot the decomposed components
+autoplot(esp_mstl2) +
+  ggtitle("(BC transformed) Multiple STL Decomposition of Daily Mean Spot Prices (2019-2024)") +
+  labs(x = "Time", y = "Daily Mean Spot Price (EUR)") +
+  theme_options
+################################################################################
 
 
 # Augmented Dicket Fuller Test
 adf.test(esp_ts)
 
-# Jarque Bera Test
-jarque.bera.test(esp_ts)
+adf.test(esp_boxcox)
+
 
 # ACF plot
 ggAcf(esp_ts, main = "ACF Plot")
 
 # PACF plot
-ggPacf(esp_ts, main = "PACF Plot")
+ggpPacf(esp_ts, main = "PACF Plot")
+
+# ACF plot
+ggAcf(esp_boxcox, main = "ACF Plot")
+
+# PACF plot
+ggPacf(esp_boxcox, main = "PACF Plot")
+
+
+
+
+
 
 
 
