2d1fc9a8e65bd3861be2d24188b7a4e5c8118983
Kevi2217 <SH02KO@student.aau.dk>
2024-05-08 15:34:54 +0200

    .

--- one_step_forecast.R
+++ one_step_forecast.R
@@ -1,17 +1,43 @@
 
-n <- 5
-ts_data <- esp_boxcox
+# One-step forecasting
 
-for(i in 1:n){
+# Merging training and test data.
+all_data <- c(esp_boxcox, espnew_ts)
+
+n = length(esp_boxcox)
+m = length(espnew_ts)
+
+one_pred = rep(0,m)
+one_upper = rep(0,m)
+one_lower = rep(0,m)
+
+
+for (i in 1:m) {
+  current_data = all_data[-((n+i):(n+m))]
   
-  model <- arima(ts_data, order = c(3,1,2), seasonal = list(order  = c(4,1,1), periods = 7))
+  current_fit = arima(current_data, order = c(1,0,2), include.mean = FALSE)
   
-  # One-step forecast
-  forecast_result <-forecast(model, h = 1)
+  one_pred[i] = forecast(current_fit, h = 1)$mean 
   
-  # Extract the one-step forecasted value
-  one_step_forecast <- forecast_result$mean[1]
+  one_upper[i] = forecast(current_fit, h = 1)$upper
   
-  ts_data <- window(esp_boxcox, end = end(esp_boxcox) + 1)
-  ts_data[length(ts_data)] <- one_step_forecast
+  one_lower[i] = forecast(current_fit, h = 1)$lower
 }
+
+onestepts <- ts(OneStepPredictions, start = start(espnew_ts), frequency = 365)
+
+
+autoplot(espnew_ts) +
+  autolayer(onestepts, series = "One-Step Forecast") +
+  geom_ribbon(aes(ymax = ts(OneUpper, start = start(espnew_ts),frequency = 365), ymin = ts(OneLower, start = start(espnew_ts),frequency = 365)), alpha = 0.3) +
+  ylab("Your Y-Axis Label") +
+  xlab("Your X-Axis Label") +
+  ggtitle("One-Step Forecast with Upper and Lower Bounds")
+
+
+
+onestep_back <- InvBoxCox(onestepts,  lambda)
+onestep_back <- diffinv(onestep_back, lag = 7, difference = 1)
+onestep_back <- diffinv(onestep_back, lag = 1, difference = 1)
+
+
