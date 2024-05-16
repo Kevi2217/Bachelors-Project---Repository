bd67a6771d0fb3598bde5f977a88fde5b7fb8e7c
Freddy D <93995722+HunterFreddy69@users.noreply.github.com>
2024-05-16 14:14:40 +0200

    Add files via upload

--- r_code/main.R
+++ r_code/main.R
@@ -24,6 +24,7 @@ library(gridExtra)
 library(pracma)
 library(solitude)
 library(astsa)
+library(rugarch)
 
 # OR Operator
 # |
@@ -31,6 +32,8 @@ library(astsa)
 # Henter data fra 2022 og 2023
 elspotprices_14_19_data <- read_excel("elspotprices_14-19.xlsx")
 elspotprices_19_24_data <- read_excel("elspotprices_19-24.xlsx")
+# The below is for later when testing models
+elspotprices_24_test_data <- read_excel("elspotprices_2024.xlsx")
 
 # Theme definition
 theme_options <- theme(
@@ -48,7 +51,13 @@ esp <- elspotprices_14_19_data %>%
 
 # source("seasonal_plots.R")
 
-
+esp_test <- elspotprices_24_test_data %>%
+  rename_all(tolower) %>% 
+  dplyr::mutate(hourdk = as.Date(hourdk)) %>%
+  # Already filtered for at the website, so its a sanity check
+  dplyr::filter(pricearea == "DK1") %>% 
+  dplyr::group_by(hourdk) %>%
+  dplyr::summarise(daily_mean_spotpriceeur = round(mean(spotpriceeur), 2))
 
 esp_daily <- esp %>%
   dplyr::mutate(hourdk = as.Date(hourdk)) %>%
@@ -88,9 +97,13 @@ ggplot(esp_30day_rolling_avg_daily, aes(x = hourdk,
   theme_minimal()
 
 # Converts dataframe to time series
-esp_ts <- ts(esp_daily$daily_mean_spotpriceeur, frequency = 7,
+esp_ts <- ts(esp_daily$daily_mean_spotpriceeur, frequency = 365,
              start = 2019)
 autoplot(esp_ts)
+
+#Also define our esp_test_ts which will be used in forecasting
+esp_test_ts <- ts(esp_test$daily_mean_spotpriceeur, frequency = 365,
+                  start = 2024)
 
 ############################# STL of time series ###############################
 esp_mstl2 <- mstl(msts(esp_ts, seasonal.periods = c(7, 365)), iterate = 50)
@@ -104,8 +117,12 @@ autoplot(esp_mstl2) +
 
 # Using boxcox transformation
 lambda <- BoxCox.lambda(esp_ts + abs(1.5*min(esp_ts)))
-esp_boxcox <- BoxCox(esp_ts + abs(1.5*min(esp_ts)), lambda) %>% diff() %>% diff(lag = 7)
+esp_boxcox <- BoxCox(esp_ts + abs(1.5*min(esp_ts)), lambda)
 autoplot(esp_boxcox)
+
+esp_test_boxcox <- BoxCox(esp_test_ts + abs(1.5*min(esp_ts)), lambda)
+autoplot(esp_test_boxcox)
+
 
 ##################### OUTLIER REMOVAL USING ISOLATIONFOREST #################### 
 bc_df <- as.data.frame(esp_boxcox)
@@ -114,12 +131,12 @@ iforest$fit(bc_df)
 
 #predict outliers within dataset
 bc_df$pred <- iforest$predict(bc_df)
-bc_df$outlier <- as.factor(ifelse(bc_df$pred$anomaly_score >=0.80, "outlier", "normal"))
-
-
-for (i in which(bc_df$pred$anomaly_score >= 0.8)) {
-  # Assign the value of the previous observation to the current index
-  esp_boxcox[i] <- esp_boxcox[i - 1]
+bc_df$outlier <- as.factor(ifelse(bc_df$pred$anomaly_score >=0.78, "outlier", "normal"))
+ 
+for (i in which(bc_df$pred$anomaly_score >= 0.78)) {
+  # Assign the value of the previous 7 observations
+  esp_boxcox[i] <- mean(esp_boxcox[(i - 6):i])
+  bc_df$pred$anomaly_score[i] <- 0
 }
 autoplot(esp_boxcox)
 ################################################################################
@@ -139,27 +156,108 @@ ggAcf(esp_ts, main = "ACF Plot")
 ggPacf(esp_ts, main = "PACF Plot")
 
 # ACF plot
-ggAcf(esp_boxcox, main = "ACF Plot")
+ggAcf(esp_boxcox, main = "ACF Plot", lag.max = 50)
 # PACF plot
-ggPacf(esp_boxcox, main = "PACF Plot")
+ggPacf(esp_boxcox, main = "PACF Plot", lag.max = 50)
 
-best_AICc <- Inf
-best_order <- c(0, 0)
-# Loop through all combinations of q and p
-for (q in 1:10) {
-  for (p in 1:10) {
-    # Fit ARMA model
-    arima_model <- Arima(esp_boxcox, order = c(q, 0, p))
-    
-    # Calculate AIC score
-    current_AICc <- AIC(arima_model) + (2*(p + q)*(p + q + 1))/(arima_model$nobs - (p + q) - 1)
-    
-    # Update best AIC score and order if current is better
-    if (current_AICc < best_AICc) {
-      best_AICc <- current_AICc
-      best_order <- c(q, p)
-    }
-  }
-  print(paste("Best AICc:", round(best_AICc, 2)))
-  print(best_order)
+##############################################################  GARCH  ########################################################
+# seasonal arima = 8, 1, 0
+archTest(esp_boxcox)
+
+sarima_model <- stats::arima(esp_boxcox, order = c(1,1,3), seasonal = list(order = c(8,1,0), periods = 7))
+sarima_residuals <- sarima_model$residuals
+
+                         
+# Step 3: Fit GARCH Model
+garch_spec <- ugarchspec(
+  variance.model = list(model = "sGARCH", garchOrder = c(2, 1)),
+  mean.model = list(armaOrder = c(0, 0), include.mean = FALSE)
+)
+garch_fit <- ugarchfit(spec = garch_spec,
+                       data = sarima_residuals)
+
+sarima_garch_model <- list(sarima_model1 = sarima_model, garch_model1 = garch_fit)
+
+
+# SARIMA forecast
+sarima_forecasts <- forecast(sarima_garch_model$sarima_model1, h = length(esp_test_boxcox))
+
+# GARCH forecast
+garch_forecasts <- ugarchforecast(sarima_garch_model$garch_model1, n.ahead = length(esp_test_boxcox))
+
+# LONG FORECAST ALL DAYS
+merged_boxcox_ts <- ts(c(esp_boxcox, esp_test_boxcox), start = 2019, frequency = 365)
+merged_ts <- ts(c(esp_ts, esp_test_ts), start = 2019, frequency = 365)
+
+autoplot(merged_boxcox_ts) +
+  autolayer(sarima_forecasts$mean) +
+  geom_ribbon(data = esp_test_boxcox, aes(ymin = sarima_forecasts$mean - 1.96 * sigma(garch_forecasts),
+                                          ymax = sarima_forecasts$mean + 1.96 * sigma(garch_forecasts)),
+              alpha = 0.3) +
+  ylab("EUR/MWh") +
+  ggtitle("One-Step SARIMA-GARCH Forecast with Upper and Lower Bounds") +
+  coord_cartesian(xlim = c(2023.6, 2024.4))
+
+
+# One-step forecasting
+n <- length(esp_boxcox)
+m <- length(esp_test_boxcox)
+
+onestep_pred <- rep(0,m)
+onestep_upper <- rep(0,m)
+onestep_lower <- rep(0,m)
+
+# for-loop of one-step predictions
+for (i in 1:m) {
+  current_data <- merged_boxcox_ts[-((n+i):(n+m))]
+ 
+  current_sarima_fit <- stats::arima(current_data, order = c(1, 1, 3), seasonal = list(order = c(8, 1, 0), periods = 7))
+  
+  current_garch_fit <- ugarchforecast(ugarchfit(spec = garch_spec, data = current_sarima_fit$residuals),
+                 n.ahead = 1)
+  
+  onestep_pred[i] <- forecast(current_sarima_fit, h = 1)$mean 
+  
+  onestep_upper[i] <- onestep_pred[i] + 1.96 * current_garch_fit@forecast$sigmaFor[1]
+  
+  onestep_lower[i] <- onestep_pred[i] - 1.96 * current_garch_fit@forecast$sigmaFor[1]
 }
+# Defines vectors into ts
+onestep_pred_ts <- ts(onestep_pred, start = 2024, frequency = 365)
+rscale_onestep_pred_ts <- InvBoxCox(onestep_pred_ts, lambda) - abs(1.5*min(esp_ts))
+
+onestep_upper_ts <- ts(onestep_upper, start = 2024, frequency = 365)
+rscale_onestep_upper_ts <- InvBoxCox(onestep_upper_ts, lambda) - abs(1.5*min(esp_ts))
+
+onestep_lower_ts <- ts(onestep_lower, start = 2024, frequency = 365)
+rscale_onestep_lower_ts <- InvBoxCox(onestep_lower_ts, lambda) - abs(1.5*min(esp_ts))
+
+autoplot(merged_ts) +
+  autolayer(rscale_onestep_pred_ts) +
+  geom_ribbon(data = esp_test_boxcox, aes(ymin = rscale_onestep_lower_ts,
+                                          ymax = rscale_onestep_upper_ts),
+              alpha = 0.3) +
+  ylab("EUR/MWh") +
+  ggtitle("One-Step SARIMA-GARCH Forecast with Upper and Lower Bounds") +
+  coord_cartesian(xlim = c(2023.8, 2024.4))
+
+
+# Count every observation outside the prediction interval.
+count <- 0
+indekscount <- list()
+
+for(i in seq_along(esp_test_ts)){
+  if(rscale_onestep_upper_ts[i] < esp_test_ts[i] | esp_test_ts[i] < rscale_onestep_lower_ts[i]){
+    count = count + 1
+    indekscount <- append(indekscount, i)
+  }
+}
+
+
+
+
+
+
+
+
+
