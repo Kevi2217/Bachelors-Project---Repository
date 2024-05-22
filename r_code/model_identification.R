41fe48cb84950ca14bd812bfab57594782eb3e2c
Kevi2217 <89939902+Kevi2217@users.noreply.github.com>
2024-05-22 22:57:10 +0200

    Add files via upload
    .

---
+++ r_code/model_identification.R
@@ -0,0 +1,53 @@
+#### MODEL IDENTIFICATION ####
+# Define ranges for p, d, q, P, D, Q parameters
+p_range <- 0:5
+d <- 1
+q_range <- 0:5
+P_range <- 0:8
+D <- 1
+Q_range <- 0:3
+
+# List of best AIC,AICC, BIC and SARIMA orders
+current_aic <- 0
+best_aic <- list(Inf)
+best_aicc <- list(Inf)
+best_bic <- list(Inf)
+optimal_orders <- list()
+optimal_seasonal_orders <- list()
+
+for (p in p_range) {
+  for (q in q_range) {
+    for (P in P_range) {
+      for (Q in Q_range) {
+        # Fit SARIMA model for each combination
+        sarima_model <- arima(esp_only, order = c(p,d,q), method = "ML",
+                              seasonal = list(order = c(P,D,Q), periods = 7))
+        # Check if model fitting was successful and evaluate based on AIC
+        if (!is.null(sarima_model)) {
+          n <- length(sarima_model$residuals)
+          k <- length(sarima_model$coef)
+          current_aic <-  sarima_model$aic
+          current_aicc <- sarima_model$aic + (2 * k * (k + 1)) / (n - k - 1)
+          current_bic <- BIC(sarima_model)
+          if (current_aic < tail(best_aic, n=1)) {
+            best_aicc <- append(best_aicc, current_aicc)
+            best_aic <- append(best_aic, current_aic)
+            best_bic <- append(best_bic, current_bic)
+            optimal_orders <- append(optimal_orders,c(p, d, q))
+            optimal_seasonal_orders <- append(optimal_seasonal_orders,c(P, D, Q))
+          }
+        }
+      }
+    }
+  }
+}
+
+#Given model.
+testmodel <- arima(esp_only, order = c(1,1,3), seasonal = list(order = c(8,1,0), periods = 7))
+
+summary(testmodel)
+
+autoplot(testmodel)
+
+#fit plot 
+autoplot(esp_only) + autolayer(fitted(testmodel))
---
+++ r_code/ts_data.R
@@ -0,0 +1,57 @@
+library(dplyr)
+library(scales)
+library(tidyr)
+library(lubridate)
+library(tseries)
+library(MASS)
+library(forecast)
+library(ggplot2)
+library(gridExtra)
+library(LSTS)
+library(MLmetrics) 
+library(Metrics)
+
+# Training data
+elspotprices_19_24 <- elspotprices_19_24 %>% 
+  rename_all(tolower)
+
+esp <- elspotprices_19_24 %>%
+  dplyr::mutate(hourdk = as.Date(hourdk)) %>%
+  dplyr::group_by(pricearea, hourdk) %>%
+  dplyr::summarise(DAP_DKK = round(mean(spotpricedkk), 2),
+                   DAP_EUR = round(mean(spotpriceeur), 2)) %>% 
+  dplyr::filter(pricearea == "DK1")
+
+
+# Format the newer data points.
+elspotprices_2024_to_may <- elspotprices_2024_to_may %>% 
+  rename_all(tolower)
+
+elspot_new <- elspotprices_2024_to_may %>% 
+  dplyr::mutate(spotpricedkk = as.numeric(gsub(",",".",spotpricedkk, fixed = TRUE))) %>% 
+  dplyr::mutate(spotpriceeur = as.numeric(gsub(",",".",spotpriceeur, fixed = TRUE))) %>% 
+  dplyr::mutate(hourdk = as.Date(hourdk))
+
+elspot_new <- elspot_new %>% 
+  dplyr::group_by(pricearea, hourdk) %>%
+  dplyr::summarise(DAP_DKK = round(mean(spotpricedkk), 2),
+                   DAP_EUR = round(mean(spotpriceeur), 2)) %>% 
+  dplyr::filter(pricearea == "DK1")
+
+
+# Convert into time series object
+espnew_ts <- ts(elspot_new$DAP_EUR, start = end(esp_ts), frequency = 365)
+esp_ts <- ts(esp$DAP_EUR, start = 2019, frequency = 365)
+
+
+# Box Cox-transform and difference the time series
+lambda <- BoxCox.lambda(esp_ts + abs(1.5*min(esp_ts)))
+esp_boxcox_diff <- BoxCox(esp_ts + abs(1.5*min(esp_ts)), lambda) %>% diff() %>% diff(lag = 7)
+
+esp_only <- BoxCox(esp_ts + abs(1.5*min(esp_ts)), lambda)
+# Changing the 'outliers'
+for (i in seq_along(esp_only)) {
+  if (esp_only[i] < 0.985) {
+    esp_only[i] <- mean(esp_only[max(1, i - 6):i])
+  }
+}
